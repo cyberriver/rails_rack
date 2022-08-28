d0a4b9940d86796eb846ec701334eedea25245ef
cyberriver <vblagirev@EMAIL.com>
2022-08-28 23:25:52 +0300

    third refactoring

--- app.rb
+++ app.rb
@@ -1,45 +1,28 @@
 require 'rack'
-require_relative 'format'
+require_relative 'time_formatter'
 
 class App
-include Format
 
   def call(env)
     request = Rack::Request.new(env)
     #filtering request by incorrect parameters
     if request.params != {}
-      main_check(request) 
-    else
-      incorrect_parameters("Request has no parameters"+"\n")    
+      time_formatter = TimeFormatter.new(request)            
+      if time_formatter.invalid?
+        response(time_formatter.call,404,headers)
+      else 
+        reponse(time_formatter.call,200,headers)
+      end
+    else      
+      response("Request has no parameters"+"\n", 400, headers)    
     end
 
   end
 
   private
   #filtering request by incorrect parameters
-  def main_check(request)
-    if check_params(request)
-      set_date_directives(split_params(request.params["format"]))
-      serve_request(request.params)
-    else
-      incorrect_parameters(@diff) #filtering request by incorrect parameters
-    end
-  end
-
-  def check_params(request)
-    params = request.params
-    verify_params(params["format"]) if has_format?(params) if params.size >0
-  end
-
-  def serve_request(params)  
-    msg = Time.now.strftime(params.join('-')).to_s
-    response(msg, 200,{ "Content-Type" => "text/plain" })  
-    
-  end
-
-  def incorrect_parameters(args)
-    msg = "Unknown time format #{args}"+"\n"
-    response(msg, 400, { "Content-Type" => "text/plain" })
+  def headers
+    { "Content-Type" => "text/plain" }    
   end
 
   def response (body, status, headers)
